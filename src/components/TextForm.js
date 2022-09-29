4f821bed4fae7c62f29d8977fc9a50f532e82991
Rittik Kumar <rkrittik17@gmail.com>
2022-09-29 13:50:39 +0530

    some updates

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -38,27 +38,27 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
     return (
         <>
             <div className='container' style={{color: props.mode === 'dark'? 'white':'black'}}>
-                <h1>{props.heading}</h1>
+                <h1 className='mb-4'>{props.heading}</h1>
                 <div className="mb-3">
                     <textarea className="form-control" value={text} style={{backgroundColor: props.mode === 'light'? 'white':'#18202b', color: props.mode === 'dark'? 'white':'black'}} onChange={handleOnChange} id="myBox" rows="10"></textarea>
                 </div>
-                <button className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2`} onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2`} onClick={handleLoClick}>Convert to Lowercase</button>
-                <button className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2`} onClick={handleCopy}>Copy Text</button>
-                <button className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2`} onClick={handleExtraSpaces}>Remove extra spaces</button>
-                <button className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2`} onClick={handleClearClick}>Clear Text</button>
+                <button disabled={text.length===0} className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2 my-2`} onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length===0} className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2 my-2`} onClick={handleLoClick}>Convert to Lowercase</button>
+                <button disabled={text.length===0} className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2 my-2`} onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length===0} className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2 my-2`} onClick={handleExtraSpaces}>Remove extra spaces</button>
+                <button disabled={text.length===0} className={`btn btn-${props.mode === 'light'? 'dark':'light'} mx-2 my-2`} onClick={handleClearClick}>Clear Text</button>
                 
             </div>
             <div className='container my-3' style={{backgroundColor: props.mode === 'light'? 'white':'#18202b', color: props.mode === 'dark'? 'white':'black'}}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{0.008*text.split(" ").length} minutes read</p>
+                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+                <p>{text.length>0?text:"Nothing to preview"}</p>
             </div>
         </>
   )
